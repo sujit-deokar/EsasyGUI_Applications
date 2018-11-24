ae8339fb8647302aa405b92f6d128bd86f3dc7b6
Sujit Deokar <sujitdeokar30@gmail.com>
2018-11-24 14:32:07 +0530

    Update Image_viewer_Easygui.py

--- Image_viewer_Easygui.py
+++ Image_viewer_Easygui.py
@@ -18,14 +18,14 @@ from easygui import diropenbox, buttonbox
 import glob
 state = True
 j=0
-path = diropenbox(msg=None, title=None, default='/home/sujit/Dell_Inspiron/dataset/CASIA-WebFace')
+path = diropenbox(msg=None, title=None, default='./folder path')
 message = 'Image Viewer - Using Easygui'
 choice = ('Previous', 'Next', 'Exit')
-image_set = glob.glob(path + '/*.jpg') 
+image_set = glob.glob(path + '/*.jpg')                                                          # can take any image extension
 choice_select = buttonbox(msg=message, title=' ', choices=choice, image=image_set[0])
 while state:
     if choice_select == 'Next':
-        if j< len(image_set)-1:
+        if j< (len(image_set)-1):
             j=j+1
             choice_select = buttonbox(msg=message, title=' ', choices=choice, image=image_set[j])
         else:
